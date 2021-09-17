c3b6e834678df40cdc83dfc398b3f4a6780fcc91
Minun <minun@mewmew.cn>
2021-09-17 10:08:31 +0800

    add missing file

---
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentRecord.java
@@ -0,0 +1,19 @@
+package minun.zte.axon30.under_screen_adjustment;
+
+import java.util.Date;
+
+public class AdjustmentRecord {
+
+    public float time;
+
+    public float temperature;
+    public float brightness;
+
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+
+    public float notch;
+
+}
